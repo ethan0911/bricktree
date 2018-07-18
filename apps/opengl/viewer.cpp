12bd90c576fc0c3f1dc0568d85366aedd37d6568
Feng <fwang911@gmail.com>
2018-07-17 21:35:55 -0600

    fix value brick boundary and optimize the performance

--- apps/opengl/viewer.cpp
+++ apps/opengl/viewer.cpp
@@ -149,7 +149,7 @@ void WidgetDraw() {
     ImVec2 window_pos_pivot = ImVec2((corner & 1) ? 1.0f : 0.0f, 
                                      (corner & 2) ? 1.0f : 0.0f);
     ImGui::SetNextWindowPos(window_pos, ImGuiCond_Always, window_pos_pivot);
-    ImGui::PushStyleColor(ImGuiCol_WindowBg, ImVec4(0.0f, 0.0f, 0.0f, 0.3f));
+    ImGui::PushStyleColor(ImGuiCol_WindowBg, ImVec4(0.0f, 0.0f, 0.0f, 0.5f));
     if (ImGui::Begin("Metrics", &show_app_tfn, 
                      ImGuiWindowFlags_NoTitleBar|
                      ImGuiWindowFlags_NoResize|
